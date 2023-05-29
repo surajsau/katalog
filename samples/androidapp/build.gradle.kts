51fefb792f2d81d2e527faa40412c3c8e7686040
surajsau <suji.in.japan@gmail.com>
2023-05-30 07:55:54 +0900

    Added Compose  Material 3 library

--- samples/androidapp/build.gradle.kts
+++ samples/androidapp/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation(libs.androidx.compose.material)
     implementation(libs.androidx.compose.material.icons.core)
     implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.androidx.compose.material3)
 
     implementation(libs.material)
 }
